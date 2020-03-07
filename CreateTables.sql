2eb52d4fa50c72d747407502a970ae5429601d71
Der Chien <b03902015@ntu.edu.tw>
2020-03-08 00:44:58 +0800

    add sql script to create tables

---
+++ CreateTables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE "users" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"account"			VARCHAR(255),
+	"password"			VARCHAR(255),
+	"name"				VARCHAR(255),
+	"type"				VARCHAR(255),
+	"location"			VARCHAR(255),
+	"motto"				VARCHAR(255),
+	"intro"				VARCHAR(2047),
+	"url_avatar"		VARCHAR(255),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "boards" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"				VARCHAR(255),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "posts" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"id_user"			INTEGER,
+	"id_board"			INTEGER,
+	"title"				VARCHAR(255),
+	"content"			VARCHAR(2047),
+	"url_avatar"		VARCHAR(255),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "comments" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"id_user"			INTEGER,
+	"id_post"			INTEGER,
+	"content"			VARCHAR(2047),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "chatrooms" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"id_user"			INTEGER,
+	"id_user_target"	INTEGER,
+	"name"				VARCHAR(255),
+	"url_avatar"		VARCHAR(255),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "messages" (
+	"id"				INTEGER PRIMARY KEY AUTOINCREMENT,
+	"id_chatroom"		INTEGER,
+	"status"			INTEGER,
+	"content"			VARCHAR(255),
+	"ts_create"			DATETIME DEFAULT CURRENT_TIMESTAMP
+);
